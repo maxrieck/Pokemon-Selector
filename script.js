b8d3c42228c0bf92b6011236a53ce83cfea98825
Max Rieck <maxermusrieck@gmail.com>
2025-05-02 08:55:29 -0700

    Added second image

--- script.js
+++ script.js
@@ -19,25 +19,31 @@ const pokemonData = async (api, text) => {
         const typeName = types[0].type.name;   
         const iconImg = sprites.front_default;     
 
-        //<img src="${iconImg}" alt="${name}"> 
+         
 
         setTimeout(() => {
             result.innerHTML = `
             <div id="pokemon-card" class="container mt-5">
             
-            <div class="row">
+            <div class="row text-center">
             <h1 id="pokemon-name">${name}</h1>
             <h3>#${id}</h3>
             </div>
 
-            <div class="row">
+            <div class="row mx-auto p-2">
             <img src="${front_default}" id="card-img" alt="${name}"> 
             </div>
 
+            <div class="row">
+            <div class="column col-6">
             <h4 id="pokemon-type">Type: ${typeName}</h4>
             <h5>Height: ${height}"</h5>
             <h5>Weight: ${weight}lbs</h5>
-            
+            </div>
+            <div class="column col-6">
+            <img src="${iconImg}" class="border border-secondary-subtle rounded-circle" alt="${name}">
+            </div>
+            </div>
             </div>
             
             `;
@@ -46,9 +52,26 @@ const pokemonData = async (api, text) => {
     } catch (error) {
         console.log(error)
 
+       
+
         setTimeout(() => {
             result.innerHTML = `
+            
+            <div id="pokemon-card" class="container mt-5">
+            
+            <div class="row text-center">
             <h1>Pokemon not found</h1>
+            </div>
+
+            <div class="row mx-auto p-2">
+            <img src="assets/psyduck-deliciousdaywithpokemon.png" id="card-img" alt="Image not found"> 
+            </div>
+
+            <div class="row">
+            <h4 id="pokemon-type">Make sure to spell the name correctly, or search by typing 1 through 150*.</h4>
+            <h6>*Pokemon after the orginal 150 don't matter.</h6>
+            </div>
+            </div>
             
             `;
         }, 1500)
